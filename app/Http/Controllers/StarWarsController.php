8bcb47276001e7c07d9c05c2e8511f98f0871410
Harry Chow <harry@harrychow.com>
2024-03-20 03:12:07 -0700

    - Moved url into const and added some comments

--- app/Http/Controllers/StarWarsController.php
+++ app/Http/Controllers/StarWarsController.php
@@ -8,15 +8,33 @@ use GuzzleHttp\Client;
 
 class StarWarsController extends Controller
 {
+    /**
+     * API Endpoint for Starwars API
+     */
+    const STARWARS_PEOPLE_API = 'https://swapi.dev/api/people/';
+
+    /**
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View|\Illuminate\Foundation\Application
+     */
     public function index()
     {
         return view('starwars.index');
     }
 
+    /**
+     *
+     * GET request to search starwars characters
+     *
+     * Search via name provided by the 'search' input from REQUEST
+     *
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
     public function search(Request $request)
     {
         $client = new Client();
-        $response = $client->request('GET', 'https://swapi.dev/api/people/', [
+        $response = $client->request('GET', self::STARWARS_PEOPLE_API, [
             'query' => ['search' => $request->input('search')]
         ]);
 
